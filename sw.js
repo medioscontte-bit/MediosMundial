8a0992afbd8b6b4beb26a39f99accc8bab5ab6a6
Medios Taller <taller@medios.edu.ar>
2026-06-05 21:11:44 -0300

    Simulador Mundial 2026 - version inicial

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'mundial2026-v1';
+const URLS = ['index.html','manifest.json','icon-192.png','icon-512.png','logo-colegio.jpg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('Offline', {status: 503})))
+  );
+});
